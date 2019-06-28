90722f5da07332db63afc026c516395658c66346
libi <7769922+libi@users.noreply.github.com>
2019-06-28 15:36:08 +0800

    a lot of motify

---
+++ driver/redis_driver_test.go
@@ -0,0 +1,26 @@
+package driver
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRedisDriver_Scan(t *testing.T) {
+	rd := new(RedisDriver)
+	rd.Open(DriverConnOpt{
+		Host:"127.0.0.1",
+		Port:"6379",
+		Password:"",
+	})
+
+	testStr := []string{
+		"*","-----","","!@#$%^","1","false",
+	}
+	for _,str := range testStr{
+		ret,err := rd.Scan(str)
+		if(err != nil){
+			panic(err)
+		}
+		fmt.Println(ret)
+	}
+}
