e1f78c4a27f3eb1dc6ef23aeff8ea6529800f612
xuyun <xuyun@windimg.com>
2022-06-24 17:48:43 +0800

    1. keepalive移到SetHeartBeat里面 2. 去掉不重要log 3. NewEtcdDriver参数直接用*clientv3.Config

--- driver/etcd/etcd_driver.go
+++ driver/etcd/etcd_driver.go
@@ -20,22 +20,16 @@ const (
 )
 
 type EtcdDriver struct {
-	cli           *clientv3.Client
-	lease         int64
-	leaseID       clientv3.LeaseID
-	keepAliveChan <-chan *clientv3.LeaseKeepAliveResponse
-	serverList    map[string]map[string]string
-	lock          sync.Mutex
+	cli        *clientv3.Client
+	lease      int64
+	serverList map[string]map[string]string
+	lock       sync.Mutex
 }
 
 //NewEtcdDriver ...
-func NewEtcdDriver(endpoints []string) (*EtcdDriver, error) {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   endpoints,
-		DialTimeout: dialTimeout,
-	})
+func NewEtcdDriver(config *clientv3.Config) (*EtcdDriver, error) {
+	cli, err := clientv3.New(*config)
 	if err != nil {
-		log.Printf("NewEtcdDriver error: %v", err)
 		return nil, err
 	}
 
@@ -47,8 +41,8 @@ func NewEtcdDriver(endpoints []string) (*EtcdDriver, error) {
 	return ser, nil
 }
 
-//设置租约
-func (s *EtcdDriver) putKeyWithLease(key, val string) error {
+//设置key value，绑定租约
+func (s *EtcdDriver) putKeyWithLease(key, val string) (clientv3.LeaseID, error) {
 	//设置租约时间，最少5s
 	if s.lease < defaultLease {
 		s.lease = defaultLease
@@ -59,50 +53,20 @@ func (s *EtcdDriver) putKeyWithLease(key, val string) error {
 
 	resp, err := s.cli.Grant(ctx, s.lease)
 	if err != nil {
-		log.Printf("grant error:%v", err)
-		return err
+		return 0, err
 	}
+	leaseID := resp.ID
 	//注册服务并绑定租约
-	_, err = s.cli.Put(ctx, key, val, clientv3.WithLease(resp.ID))
+	_, err = s.cli.Put(ctx, key, val, clientv3.WithLease(leaseID))
 	if err != nil {
-		log.Printf("put error:%v", err)
-		return err
+		return 0, err
 	}
-	//设置续租 定期发送需求请求 此处ctx不能设置超时
-	leaseRespChan, err := s.cli.KeepAlive(context.Background(), resp.ID)
 
-	if err != nil {
-		log.Printf("keepalive error:%v", err)
-		return err
-	}
-	s.leaseID = resp.ID
-	s.keepAliveChan = leaseRespChan
-	log.Printf("putKeyWithLease key:%v  val:%v  success!", key, val)
-	return nil
+	return leaseID, nil
 }
 
-//listenLeaseRespChan 监听 续租情况
-func (s *EtcdDriver) listenLeaseRespChan() {
-	for leaseKeepResp := range s.keepAliveChan {
-		log.Printf("续约成功  %v", leaseKeepResp)
-	}
-	log.Printf("关闭续租")
-}
-
-// Close 注销服务
-func (s *EtcdDriver) Close() error {
-	//撤销租约
-	if _, err := s.cli.Revoke(context.Background(), s.leaseID); err != nil {
-		return err
-	}
-	log.Printf("撤销租约")
-	return s.cli.Close()
-}
-
-func (s *EtcdDriver) randNodeID(serviceName string) (path, nodeID string) {
-	nodeID = uuid.New().String()
-	path = getPrefix(serviceName) + nodeID
-	return
+func (s *EtcdDriver) randNodeID(serviceName string) (nodeID string) {
+	return getPrefix(serviceName) + uuid.New().String()
 }
 
 //WatchService 初始化服务列表和监视
@@ -131,7 +95,6 @@ func getPrefix(serviceName string) string {
 func (s *EtcdDriver) watcher(serviceName string) {
 	prefix := getPrefix(serviceName)
 	rch := s.cli.Watch(context.Background(), prefix, clientv3.WithPrefix())
-	log.Printf("watching prefix:%s now...", prefix)
 	for wresp := range rch {
 		for _, ev := range wresp.Events {
 			switch ev.Type {
@@ -148,7 +111,6 @@ func (s *EtcdDriver) watcher(serviceName string) {
 func (s *EtcdDriver) setServiceList(serviceName, key, val string) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	log.Printf("put key :%v val:%v", key, val)
 	var nodeMap map[string]string
 	var ok bool
 	if nodeMap, ok = s.serverList[serviceName]; !ok {
@@ -162,7 +124,6 @@ func (s *EtcdDriver) setServiceList(serviceName, key, val string) {
 func (s *EtcdDriver) delServiceList(serviceName, key string) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
-	log.Printf("del serviceName:%v, key:%v", serviceName, key)
 	if nodeMap, ok := s.serverList[serviceName]; ok {
 		delete(nodeMap, key)
 	}
@@ -186,7 +147,29 @@ func (e *EtcdDriver) Ping() error {
 }
 
 func (e *EtcdDriver) SetHeartBeat(nodeID string) {
-	//no need
+
+	leaseID, err := e.putKeyWithLease(nodeID, nodeID)
+	if err != nil {
+		log.Printf("putKeyWithLease error: %v", err)
+		return
+	}
+
+	//设置续租  此处ctx不能设置超时
+	leaseRespChan, err := e.cli.KeepAlive(context.Background(), leaseID)
+
+	if err != nil {
+		log.Printf("keepalive error:%v", err)
+		return
+	}
+
+	go func() {
+		for {
+			_ = <-leaseRespChan
+			//log.Printf("续约成功 %v", resp)
+		}
+		//log.Printf("关闭续租")
+	}()
+
 }
 
 func (e *EtcdDriver) SetTimeout(timeout time.Duration) {
@@ -199,17 +182,14 @@ func (e *EtcdDriver) GetServiceNodeList(serviceName string) ([]string, error) {
 }
 
 func (e *EtcdDriver) RegisterServiceNode(serviceName string) (string, error) {
-
-	path, nodeID := e.randNodeID(serviceName)
-
-	//申请租约设置时间keepalive
-	if err := e.putKeyWithLease(path, nodeID); err != nil {
+	nodeId := e.randNodeID(serviceName)
+	_, err := e.putKeyWithLease(nodeId, nodeId)
+	if err != nil {
 		return "", err
 	}
-
-	go e.listenLeaseRespChan()
-
-	e.watchService(serviceName)
-
-	return nodeID, nil
+	err = e.watchService(serviceName)
+	if err != nil {
+		return "", err
+	}
+	return nodeId, nil
 }
--- driver/etcd/etcd_driver_test.go
+++ driver/etcd/etcd_driver_test.go
@@ -2,6 +2,7 @@ package etcd
 
 import (
 	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"testing"
 	"time"
 )
@@ -10,30 +11,79 @@ func TestEtcdDriver(t *testing.T) {
 
 	var endpoints = []string{"127.0.0.1:2379"}
 
-	ed, err := NewEtcdDriver(endpoints)
+	ed, err := NewEtcdDriver(&clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	})
 
 	require.Nil(t, err)
 	serviceName := "testService"
 
-	nodeID, err := ed.RegisterServiceNode(serviceName)
+	nodeMap := make(map[string]string)
 
-	require.Nil(t, err)
+	count := 10
 
-	t.Logf("nodeId:%v", nodeID)
-
-	time.Sleep(time.Second * 1)
-
-	//register second node
-	ed.RegisterServiceNode(serviceName)
+	for i := 0; i < count; i++ {
+		nodeID, err := ed.RegisterServiceNode(serviceName)
+		require.Nil(t, err)
+		t.Logf("nodeId %v:%v", i, nodeID)
+		nodeMap[nodeID] = nodeID
+	}
 
 	list, err := ed.GetServiceNodeList(serviceName)
 
 	require.Nil(t, err)
 
+	require.Equal(t, count, len(list))
+
 	for _, v := range list {
-		t.Logf("item:%v", v)
+		if _, ok := nodeMap[v]; !ok {
+			t.Errorf("nodeId %v not found!!!", v)
+		}
+	}
+
+}
+
+func TestSetHeartBeat(t *testing.T) {
+
+	var endpoints = []string{"127.0.0.1:2379"}
+
+	ed, err := NewEtcdDriver(&clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	})
+
+	require.Nil(t, err)
+	serviceName := "testService"
+
+	nodeMap := make(map[string]string)
+
+	count := 10
+
+	//一半设置心跳
+	for i := 0; i < count; i++ {
+		nodeID, err := ed.RegisterServiceNode(serviceName)
+		require.Nil(t, err)
+		t.Logf("nodeId %v:%v", i, nodeID)
+		if i%2 == 0 {
+			ed.SetHeartBeat(nodeID)
+			nodeMap[nodeID] = nodeID
+		}
 	}
 
 	time.Sleep(time.Second * 10)
+
+	//10s后获取serverList，预期只能取到一半
+	list, err := ed.GetServiceNodeList(serviceName)
+
+	require.Nil(t, err)
+
+	require.Equal(t, len(nodeMap), len(list))
+
+	for _, v := range list {
+		if _, ok := nodeMap[v]; !ok {
+			t.Errorf("nodeId %v not found!!!", v)
+		}
+	}
 
 }
